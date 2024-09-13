ae9351f6dc801fba517bcf51410b33155d7819e8
felixstuart <148880497+felixstuart@users.noreply.github.com>
2024-09-13 19:14:37 -0400

    use getters & setters

--- src/CoffeeDrink.java
+++ src/CoffeeDrink.java
@@ -2,9 +2,9 @@
 
 class CoffeeDrink {
 
-    private int numberOfCups;
-    private String flavor;
-    private boolean hasWhippedCream;
+    int numberOfCups;
+    String flavor;
+    boolean hasWhippedCream;
 
     // Constructor for CoffeeDrink class
     public CoffeeDrink(int numberOfCups, String flavor, boolean hasWhippedCream) {
@@ -18,23 +18,27 @@ class CoffeeDrink {
         System.out.println("Number of Cups: " + numberOfCups + " flavor: " + flavor + (hasWhippedCream ? " has whipped cream" : " does not have whipped cream"));
     }
 
-    public int getNumberOfCups() {
-        return this.numberOfCups;
-    }
-    public String getFlavor() {
-        return this.flavor;
-    }
-    public boolean getHasWhippedCream() {
-        return this.hasWhippedCream;
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
     }
 
     public void setNumberOfCups(int numberOfCups) {
         this.numberOfCups = numberOfCups;
     }
-    public void setFlavor(String flavor) {
-        this.flavor = flavor;
-    }
+
     public void setHasWhippedCream(boolean hasWhippedCream) {
         this.hasWhippedCream = hasWhippedCream;
+    }
+
+    public int getNumberOfCups() {
+        return numberOfCups;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public boolean isHasWhippedCream() {
+        return hasWhippedCream;
     }
 }
