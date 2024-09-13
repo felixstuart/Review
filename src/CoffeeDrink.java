d2e20ee7ad14d9cf72d5f7064f1ccd1055130c31
Felix Stuart <148880497+felixstuart@users.noreply.github.com>
2024-09-13 17:23:46 -0400

    get & set\

--- src/CoffeeDrink.java
+++ src/CoffeeDrink.java
@@ -2,9 +2,9 @@
 
 class CoffeeDrink {
 
-    int numberOfCups;
-    String flavor;
-    boolean hasWhippedCream;
+    private int numberOfCups;
+    private String flavor;
+    private boolean hasWhippedCream;
 
     // Constructor for CoffeeDrink class
     public CoffeeDrink(int numberOfCups, String flavor, boolean hasWhippedCream) {
@@ -16,5 +16,25 @@ class CoffeeDrink {
     // Method to print information about the CoffeeDrink
     public void printInfo() {
         System.out.println("Number of Cups: " + numberOfCups + " flavor: " + flavor + (hasWhippedCream ? " has whipped cream" : " does not have whipped cream"));
+    }
+
+    public int getNumberOfCups() {
+        return this.numberOfCups;
+    }
+    public String getFlavor() {
+        return this.flavor;
+    }
+    public boolean getHasWhippedCream() {
+        return this.hasWhippedCream;
+    }
+
+    public void setNumberOfCups(int numberOfCups) {
+        this.numberOfCups = numberOfCups;
+    }
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+    public void setHasWhippedCream(boolean hasWhippedCream) {
+        this.hasWhippedCream = hasWhippedCream;
     }
 }
